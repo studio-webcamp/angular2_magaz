6e0cc304fbf10ed587b6c4515d450319ee7bde8a
zhukovka <helen.zhukova@gmail.com>
2016-10-30 12:21:01 +0200

    config files

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by lenka on 10/29/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>KUKURUKU</h1>'
+})
+export class AppComponent { }
---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by lenka on 10/29/16.
+ */
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }   from './app.component';
+
+@NgModule({
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
