2a6741551dd5280daaa81defae4981dfdce50e12
zhukovka <helen.zhukova@gmail.com>
2016-11-12 13:43:58 +0200

    gitignore

--- app/main.js
+++ app/main.js
@@ -4,9 +4,6 @@
  */
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
-var Magaz_1 = require("./models/Magaz");
-var magaz = new Magaz_1.default();
-magaz.getAllItems();
 var platform = platform_browser_dynamic_1.platformBrowserDynamic();
 platform.bootstrapModule(app_module_1.AppModule);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -3,8 +3,5 @@
  */
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
-import Magaz from "./models/Magaz";
-let magaz = new Magaz();
-magaz.getAllItems();
 const platform = platformBrowserDynamic();
 platform.bootstrapModule(AppModule);
